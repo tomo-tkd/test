8b6faa8c8273bbf83048b9eba35f60646db2c57c
tomo-tkd <tomoy605071@icloud.com>
2026-03-31 13:09:31 +0900

    frist

---
+++ test.cpp
@@ -0,0 +1,7 @@
+#include "test.hpp"
+
+vodi test() {
+    for (int i = 0; i < 10; ++i) {
+        // Do something
+    }
+}
---
+++ test.hpp
@@ -0,0 +1,6 @@
+#ifndef TEST_HPP_
+#define TEST_HPP_
+
+void test();
+
+#endif /* TEST_HPP_ */
